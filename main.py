cb926bb912d69849a78b8ecd60f22bc70c7ed1e3
Nux-xader <sumgmng@gmail.com>
2021-08-27 07:58:09 +0700

    initial

---
+++ main.py
@@ -0,0 +1,24 @@
+import requests, random, os
+
+log_file = "log_portable_whm.txt"
+subDo = [
+	"orgfree.com", "6te.net", "ueuo.com", "eu5.org", "noads.biz", 
+	"coolpage.biz", "freeoda.com", "freevar.com", "freetzi.com", "xp3.biz"
+	]
+
+def clr():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def banner():
+    print("""
+ █▀█ █▀█ █▀█ ▀█▀ ▄▀█ █▄▄ █░░ █▀▀
+ █▀▀ █▄█ █▀▄ ░█░ █▀█ █▄█ █▄▄ ██▄
+ 
+ █░█░█ █░█ █▀▄▀█
+ ▀▄▀▄▀ █▀█ █░▀░█  V1.0.0
+
+ Contact         : https://wa.me/+6281251389915
+ About Developer : https://github.com/Nux-xader
+ ________________________________________________
+""")
+
